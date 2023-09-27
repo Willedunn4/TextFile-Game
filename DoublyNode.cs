6f16c4fbe35c76d780625e7b9f49fb1a7794e6ae
Willedunn4 <82042225+Willedunn4@users.noreply.github.com>
2023-09-27 16:43:42 -0500

    Add files via upload

---
+++ DoublyNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFile_Game
+{
+    class DoublyNode
+    {
+        public string Result { get; set; }
+        public DoublyNode Next { get; set; }
+        public DoublyNode Prev { get; set; }
+
+        public DoublyNode(string result)
+        {
+            Result = result;
+        }
+    }
+}
