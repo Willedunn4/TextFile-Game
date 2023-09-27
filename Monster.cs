ba6d3bd798d27af1a07d0a9b7e58e122c641d805
Willedunn4 <82042225+Willedunn4@users.noreply.github.com>
2023-09-27 16:45:38 -0500

    Add files via upload

--- Monster.cs
+++ Monster.cs
@@ -10,10 +10,17 @@ namespace TextFile_Game
     class Monster
     {
         public string Type { get; set; }
-        public int HP { get; set; }
+        private int hp;
         public int MP { get; set; }
         public int AP { get; set; }
         public int DEF { get; set; }
+
+        public int HP
+        {
+            get { return hp; }
+            set { hp = Math.Max(0, value); }
+        }
+
 
         public Monster(string type, int hp, int mp, int ap, int def)
         {
